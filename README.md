2fc14244f7aeed69fb6d30505bc1b5828096445a
joaguilar <joaguilar@gmail.com>
2014-06-22 10:59:30 -0600

    Updated README.md and added error handling and additional comments to R script

--- README.md
+++ README.md
@@ -1,4 +1,51 @@
-GettingandCleaningDataCourseProject
-===================================
+# Getting and Cleaning Data #
 
-Getting and Cleaning Data Course Project
+#Course Project#
+
+## Project Description ##
+
+This project takes a data set from [http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones) and processes it to obtain a tidy dataset that contains the average of the means and standard deviations of the measurements from the original dataset.  
+
+## Contents ##
+
+This repository contains 3 files:
+
+- README.md: This file with information about the project
+- run\_analysis.R: Contains the run\_analysis() function, which processes the file from the dataset and outputs a tidy dataset with the average of the means and standard deviations of the measurements of the original files.
+- CodeBook.md: A code book that describes the variables, the data, and any transformations or work performed on the data
+
+## Requirements ##
+The functions contained in the run_analysis.R have a strong dependency on the [http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones ](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones  "Human Activity Recognition Using Smartphones") dataset. The file [https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip ](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip  "getdata-projectfiles-UCI HAR Dataset.zip") should be downloaded and opened in the same directory as the R file, so that the "UCI HAR Dataset" directory is at the same level as the script. This dataset is not included in this git repository.
+The folder structure should be:
+
+- run_analysis.R
+- UCI HAR Dataset
+ - activity_labels.txt
+ - features.txt
+ - features_info.txt
+ - test
+ - train
+
+The run\_analysis() function first checks if the "UCI HAR Dataset" directory exists in the working directory, if not it will exit with the error message *"Unable to find UCI HAR Dataset directory"*.
+
+## Running the analysis ##
+To run the analysis, make sure you set your working directory in R to a directory that contains both the run\_analysis.R file and the "UCI HAR Dataset" directory. Then load the R script using the command:
+
+    > source("run_analysis.R")
+
+Once the script is loaded, you can run the run\_analysis() function in one of two ways:
+
+
+
+1. Call the function on its own. It will generate and output file in the working directory called "dataset.txt" and will output the dataset to the R console:
+
+    > run_analysis()
+
+
+2. Call the function and store the output to a dataframe. This way you can perform further analysis on the resulting dataset. This option will also create the "dataset.txt" file in the working directory.
+
+    > df<-run_analysis() 
+ 
+## Output ##
+
+The run\_analysis() function creates a new dataframe with 180 rows and 68 columns that contain the subjectid, activity and the average of the means and standard deviations for several fields from the original **"Human Activity Recognition Using Smartphones"** dataset. It will return the dataframe as the output of the function and it will also save it to a new file called "dataset.txt" in the working directory. If there is already a file with that name in the directory, it will be overwritten.
--- run_analysis.R
+++ run_analysis.R
@@ -1,52 +1,82 @@
 #You should create one R script called run_analysis.R that does the following. 
 #Merges the training and the test sets to create one data set.
+run_analysis <- function(){
+  #First check if the dataset is present in the working directory:
+  if(!file.exists("UCI HAR Dataset")){
+    stop("Unable to find UCI HAR Dataset directory")
+  }
+  
+  
+  #load the activity names from the "activity_labels.txt" file, and convert it to a character vector:
+  y_levels<-read.table('UCI HAR Dataset/activity_labels.txt',header=FALSE)
+  y_levels_names<-as.character(y_levels[,2])
+  
+  #Next, load the three files from the test directory:
+  test_subject_test<-read.table("UCI HAR Dataset/test/subject_test.txt",header=FALSE)
+  test_x_test<-read.table("UCI HAR Dataset/test/x_test.txt",header=FALSE)
+  test_y_test<-read.table("UCI HAR Dataset/test/y_test.txt",header=FALSE)
+  
+  #And the three files from the train directory:
+  train_subject_test<-read.table("UCI HAR Dataset/train/subject_train.txt",header=FALSE)
+  train_x_test<-read.table("UCI HAR Dataset/train/x_train.txt",header=FALSE)
+  train_y_test<-read.table("UCI HAR Dataset/train/y_train.txt",header=FALSE)
+  
+  dataset_subject_test<-rbind(train_subject_test,test_subject_test)
+  dataset_x_test<-rbind(train_x_test,test_x_test)
+  dataset_y_test<-rbind(train_y_test,test_y_test)
+  
+  #Next step is to convert the activities from the y_*.txt files into their proper names from the "activity_labels.txt" file
+  #we loaded earlier. This is from the step "Uses descriptive activity names to name the activities in the data set" in the 
+  #assignment:
+  dataset_y_test<-as.factor(dataset_y_test[,1])
+  levels(dataset_y_test)<-y_levels_names
+  
+  #And a final dataset with all the rows from both files:
+  merged_dataset<-cbind(dataset_subject_test,dataset_y_test,dataset_x_test)
+  
+  #Now that we have the dataset, let's name the columns. 
+  #First load the column names for the datasets loaded from x_*.txt
+  cn<-read.table("UCI HAR Dataset/features.txt")
+  cnv<-as.character(cn[,2])
+  
+  #Assign the column names to the dataset. Note that since the first two columns come from the files subject_*.txt and y_*.txt,
+  #These two columns have to be named explicitly, in this case "Subject_ID" and "Activity_ID". The other column names come from 
+  #the "features.txt" file
+  names<-c("subjectid","activity",cnv)
+  colnames(merged_dataset)<-names
+  
+  #Extracts only the measurements on the mean and standard deviation for each measurement. 
+  mean_std_cols <- c(1,2,grep("mean\\(\\)|std\\(\\)", names(merged_dataset)))
+  merged_dataset_mean_std<-merged_dataset[,mean_std_cols]
+  
+  #Appropriately labels the data set with descriptive variable names. 
+  newNames<- vector(mode="character")
+  for(n in colnames(merged_dataset_mean_std)){
+    newNames<-c(newNames,create_new_name(n))
+  }
+  colnames(merged_dataset_mean_std)<-newNames
+  
+  
+  #Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+  tidy_data<-aggregate(.~subjectid+activity,merged_dataset_mean_std,mean)
+  
+  #And finally lets rename the columns to reflect the fact they are averages:
+  newNames<- vector(mode="character")
+  for(i in c(3:length(colnames(tidy_data)))){
+    newName<-paste("average",colnames(tidy_data)[i],sep="")
+    newNames<-c(newNames,newName)
+  }
+  colnames(tidy_data)<-c("subjectid","activity",newNames)
+  
+  #And let's output the tidy data set to a file for uploading.
+  write.table(tidy_data,file="dataset.txt",row.names=FALSE)
+  
+  tidy_data
 
-#load the activity names from the "activity_labels.txt" file, and convert it to a character vector:
-y_levels<-read.table('UCI HAR Dataset/activity_labels.txt',header=FALSE)
-y_levels_names<-as.character(y_levels[,2])
+}
 
-#Next, load the three files from the test directory:
-test_subject_test<-read.table("UCI HAR Dataset/test/subject_test.txt",header=FALSE)
-test_x_test<-read.table("UCI HAR Dataset/test/x_test.txt",header=FALSE)
-test_y_test<-read.table("UCI HAR Dataset/test/y_test.txt",header=FALSE)
-
-#And the three files from the train directory:
-train_subject_test<-read.table("UCI HAR Dataset/train/subject_train.txt",header=FALSE)
-train_x_test<-read.table("UCI HAR Dataset/train/x_train.txt",header=FALSE)
-train_y_test<-read.table("UCI HAR Dataset/train/y_train.txt",header=FALSE)
-
-dataset_subject_test<-rbind(train_subject_test,test_subject_test)
-dataset_x_test<-rbind(train_x_test,test_x_test)
-dataset_y_test<-rbind(train_y_test,test_y_test)
-
-#Next step is to convert the activities from the y_*.txt files into their proper names from the "activity_labels.txt" file
-#we loaded earlier. This is from the step "Uses descriptive activity names to name the activities in the data set" in the 
-#assignment:
-dataset_y_test<-as.factor(dataset_y_test[,1])
-levels(dataset_y_test)<-y_levels_names
-
-#And a final dataset with all the rows from both files:
-merged_dataset<-cbind(dataset_subject_test,dataset_y_test,dataset_x_test)
-
-#Now that we have the dataset, let's name the columns. 
-#First load the column names for the datasets loaded from x_*.txt
-cn<-read.table("UCI HAR Dataset/features.txt")
-cnv<-as.character(cn[,2])
-
-#Assign the column names to the dataset. Note that since the first two columns come from the files subject_*.txt and y_*.txt,
-#These two columns have to be named explicitly, in this case "Subject_ID" and "Activity_ID". The other column names come from 
-#the "features.txt" file
-names<-c("subjectid","activity",cnv)
-colnames(merged_dataset)<-names
-
-#Extracts only the measurements on the mean and standard deviation for each measurement. 
-mean_std_cols <- c(1,2,grep("mean\\(\\)|std\\(\\)", names(merged_dataset)))
-merged_dataset_mean_std<-merged_dataset[,mean_std_cols]
-#Appropriately labels the data set with descriptive variable names. 
-
-newNames<- vector(mode="character")
-for(n in colnames(merged_dataset_mean_std)){
-  newName<-n
+create_new_name <- function(name){
+  newName<-name
   # Time or Frequency:
   newName<-gsub("^t","time",newName)  
   newName<-gsub("^f","frequency",newName)  
@@ -66,21 +96,6 @@ for(n in colnames(merged_dataset_mean_std)){
   # X, Y or Z coordinates
   newName<-gsub("-X","x",newName)  
   newName<-gsub("-Y","y",newName)  
-  newName<-gsub("-Z","z",newName)  
-  #gsub("-X","x",gsub("-mean\\(\\)","mean_",gsub("Acc","acceleration_",gsub("Body","body_",gsub("^t","time_",names_cols[3], fixed=FALSE)))))
-  newNames<-c(newNames,newName)
+  newName<-gsub("-Z","z",newName)   
+  newName
 }
-colnames(merged_dataset_mean_std)<-newNames
-
-
-#Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
-tidy_data<-aggregate(.~subjectid+activity,merged_dataset_mean_std,mean)
-
-#And finally lets rename the columns to reflect the fact they are averages:
-newNames<- vector(mode="character")
-for(i in c(3:length(colnames(tidy_data)))){
-  newName<-paste("average",colnames(tidy_data)[i],sep="")
-  newNames<-c(newNames,newName)
-}
-colnames(tidy_data)<-c("subjectid","activity",newNames)
-write.table(tidy_data,file="dataset.txt",row.names=FALSE)
